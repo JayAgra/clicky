37d2a5f0549344d16b9aaa7b1c10e8ab4439298f
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-12-22 15:30:35 -0800

    v1.0.1 (3)

--- clicky/ViewController.swift
+++ clicky/ViewController.swift
@@ -8,42 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var Switch1: UISwitch!
-    @IBOutlet var Switch2: UISwitch!
-    @IBOutlet var Switch3: UISwitch!
-    @IBOutlet var Switch4: UISwitch!
     @IBOutlet var Switches: Array<UISwitch>?
-    @IBOutlet var ProgressBar: UIProgressView!
-    
-    @IBAction func littleClickButtonPress(_button: UIButton) {
-        UIImpactFeedbackGenerator(style: .light).impactOccurred();
-    }
-    
-    @IBAction func medClickButtonPress(_button: UIButton) {
-        UIImpactFeedbackGenerator(style: .medium).impactOccurred();
-    }
-    
-    @IBAction func bigClickButtonPress(_button: UIButton) {
-        UIImpactFeedbackGenerator(style: .heavy).impactOccurred();
-    }
-    
-    @IBAction func notifError(_button: UIButton) {
-        UINotificationFeedbackGenerator().notificationOccurred(.error);
-    }
-    
-    @IBAction func notifWarning(_button: UIButton) {
-        UINotificationFeedbackGenerator().notificationOccurred(.warning);
-    }
-    
-    @IBAction func notifSuccess(_button: UIButton) {
-        UINotificationFeedbackGenerator().notificationOccurred(.success);
-    }
-    
-    @IBAction func boomButton(_button: UIButton) {
-        UINotificationFeedbackGenerator().notificationOccurred(.error);
-        UINotificationFeedbackGenerator().notificationOccurred(.warning);
-        UINotificationFeedbackGenerator().notificationOccurred(.success);
-    }
+    @IBOutlet var Switches_Hard: Array<UISwitch>?
     
     @IBAction func backAction() {
         self.navigationController?.popViewController(animated: true)
@@ -53,6 +19,13 @@ class ViewController: UIViewController {
         let random: Int = Int.random(in: 1...5)
         for _ in 1...(random) {
             Switches?.randomElement()?.setOn(Bool.random(), animated: true)
+        }
+    }
+    
+    @IBAction func hardSwitchAction() {
+        let random: Int = Int.random(in: 1...10)
+        for _ in 1...(random) {
+            Switches_Hard?.randomElement()?.setOn(Bool.random(), animated: true)
         }
     }
     
